7688b5a4b12bea14cd43e7677f499946dc2303a0
Brad <brad.hankee@gmail.com>
2017-11-12 10:12:20 -0500

    Added email heroku app to github.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 app.get("/", (req, res) => {
   res.send({ hi: "Brad" });
@@ -7,3 +12,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+//client id: 158792368460-hkbski8b5mauciqu5eu6vno19d8mrtv1.apps.googleusercontent.com
+//client secret: ffRPWJ4pkfuCUxG4Wyk0JBmO
